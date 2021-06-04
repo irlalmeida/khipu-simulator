43722af52d5a1ed9b958c8f55615c41db0c86f6e
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-06-04 00:58:25 -0300

    tests-to-draw-knots

--- main.py
+++ main.py
@@ -14,23 +14,30 @@ turtle.goto(0, 300)
 turtle.write("Quipu Simulator", True, align="center", font=("Panibo", 50, "normal"))
 
 #user input
-first_strand_color = screen.textinput("Select Product", "What product are you accounting for?")
-#first_strand_color = "red"
+#first_strand_color = screen.textinput("Select Product", "What product are you accounting for?")
+first_strand_color = "red"
 
 # configure pen
 turtle.pendown()
 hand = turtle.Turtle()
 hand.pen(pencolor="white", pensize=2, speed=10)
-hand.shape("quipu-sewing.gif")
+hand.shape("src/quipu-sewing.gif")
 
 #drawing
 hand.circle(100,-90)
 hand.circle(100,180)
 hand.circle(100,-145)
 hand.pen(pencolor=first_strand_color, pensize=2, speed=1)
-hand.goto(-140, -5)
+hand.goto(-130, -5)
 hand.color(first_strand_color)
-hand.dot(10)
+hand.begin_fill()
+hand.circle(4)
+hand.rt(80)
+hand.fd(4)
+hand.circle(4)
+hand.end_fill()
+hand.fd(50)
+
 hand.penup()
 
 #second_strand_color = screen.textinput("Select Product", "What product are you accounting for?")
