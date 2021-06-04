0fb89bdce1e4ae03bd1e335a63e4372194ada221
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-06-04 00:27:54 -0300

    added-sources

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import turtle
 
-turtle.register_shape("quipu-sewing.gif")
+turtle.register_shape("src/quipu-sewing.gif")
 
 # set environment
 screen = turtle.getscreen()
 screen.bgcolor("black")
+
+#write title
+turtle.hideturtle()
 turtle.penup()
 turtle.pen(pencolor="white")
 turtle.goto(0, 300)
-turtle.write("Quipu Simulator", True, align="center", font=("Arial", 50, "bold"))
+turtle.write("Quipu Simulator", True, align="center", font=("Panibo", 50, "normal"))
 
 #user input
-#first_strand_color = screen.textinput("Select Product", "What product are you accounting for?")
-first_strand_color = "red"
+first_strand_color = screen.textinput("Select Product", "What product are you accounting for?")
+#first_strand_color = "red"
 
 # configure pen
 turtle.pendown()
 hand = turtle.Turtle()
 hand.pen(pencolor="white", pensize=2, speed=10)
 hand.shape("quipu-sewing.gif")
-
 
 #drawing
 hand.circle(100,-90)
