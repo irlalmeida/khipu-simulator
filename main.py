7d066d9019a32eec2ca13e237deea7e6c6280d01
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-07-10 21:18:40 -0300

    first-strand

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ screen.bgcolor("black")
 turtle.hideturtle()
 turtle.penup()
 turtle.pen(pencolor="white")
-turtle.goto(0, 300)
-turtle.write("Quipu Simulator", True, align="center", font=("Panibo", 50, "normal"))
+turtle.goto(0, 280)
+turtle.write("Quipu Simulator", True, align="center", font=("Panibo", 100, "normal"))
+
 
 #user input
 #first_strand_color = screen.textinput("Select Product", "What product are you accounting for?")
@@ -24,23 +25,27 @@ hand.pen(pencolor="white", pensize=2, speed=10)
 hand.shape("src/quipu-sewing.gif")
 
 #drawing
-hand.circle(100,-90)
-hand.circle(100,180)
-hand.circle(100,-145)
+hand.circle(200,-90)
+hand.circle(200,180)
+hand.circle(200,-145)
 hand.pen(pencolor=first_strand_color, pensize=2, speed=1)
-hand.goto(-130, -5)
+hand.seth(180+45)
+hand.fd(25)
 hand.color(first_strand_color)
 hand.begin_fill()
 hand.circle(4)
-hand.rt(80)
+hand.seth(180+45)
+hand.fd(4)
+hand.seth(180+45)
 hand.fd(4)
 hand.circle(4)
 hand.end_fill()
 hand.fd(50)
-
 hand.penup()
+hand.seth(0)
+hand.goto(0,0)
 
 #second_strand_color = screen.textinput("Select Product", "What product are you accounting for?")
 
-turtle.getscreen()._root.mainloop()
+#turtle.getscreen()._root.mainloop()
 
